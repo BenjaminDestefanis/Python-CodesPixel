fc5a5e920ee716d044bf99d431ef2fa1c29de1c5
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-06-29 23:44:47 -0300

    29/06/24 23:44 Hs

---
+++ chapter1/index.py
@@ -0,0 +1,11 @@
+# Comandos
+
+# python --version
+
+#You can use the Python 3 print function in Python 2 with the following import statement:
+from __future__ import print_function
+
+
+
+#$ python hello.py
+#Hello, World
